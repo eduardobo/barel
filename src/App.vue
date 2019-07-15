629546f9334e94626a1b6645f5b6cdd8009b6aae
Eduardo Barron <eduard_park.13@hotmail.com>
2019-07-14 20:20:29 -0500

    quesos barel

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,26 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+      <b-navbar b-navbar toggleable="lg" type="dark" variant="success">
+        <b-container>
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          <b-navbar-brand :to="{name:'home'}">Barel</b-navbar-brand>
+
+          <b-collapse is-nav id="nav-collapse">
+            <b-navbar-nav>
+              <b-nav-item :to="{name:'product'}" exact>Productos</b-nav-item>
+            </b-navbar-nav>
+
+            <b-navbar-nav class="ml-auto">
+              <b-nav-item :to="{name:'contact'}">Contacto</b-nav-item>
+
+            </b-navbar-nav>
+          </b-collapse>
+        </b-container>
+      </b-navbar>
+      
+    <b-container>
+      <router-view/>
+    </b-container>
+    
   </div>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
