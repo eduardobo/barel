70f4ec026b9373efc6580d75e5253de18e5adab2
Eduardo Barron <eduard_park.13@hotmail.com>
2019-07-16 22:38:58 -0500

    problemas con el tamanio de imagen, pero se arreglo el problema con los paths

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -3,23 +3,8 @@
         <h1>Productos</h1>
         <h2>Quesos</h2>
         <b-row>
-            <b-col cols="12" set="12" sm="12" md="6" lg="4" xl="4" v-for="(cheese, index) of cheeses" :key=index>
-                <b-card :title='cheese.name' :img-src="require('@/assets/logo.png')" img-top>
-                    <b-card-text>
-                        {{cheese.description}}
-                    </b-card-text>
-                </b-card>
-                <b-card :title='cheese.name' :img-src="getImagePath(cheese.image)" img-top>
-                    <b-card-text>
-                        {{cheese.description}}
-                    </b-card-text>
-                </b-card>
-                <b-card :title='cheese.name' :img-src="require(`../assets/${cheese.image}.png`)" img-top>
-                    <b-card-text>
-                        {{cheese.description}}
-                    </b-card-text>
-                </b-card>
-                <b-card :title='cheese.name' :img-src="require(`../${cheese.image2}`)" img-top>
+            <b-col class="pt-3" cols="12" set="12" sm="12" md="6" lg="4" xl="4" v-for="(cheese, index) of cheeses" :key=index>
+                <b-card :title='cheese.name' :img-src="require(`../${cheese.image}`)" img-top>
                     <b-card-text>
                         {{cheese.description}}
                     </b-card-text>
@@ -35,8 +20,10 @@ export default {
     data(){
         return{
             cheeses:[
-                {name:'Manchego', description:'Queso puramente manchego', image:'logo', image2: 'assets/logo.png' },
-                {name:'Enchilado', description:'Queso manchego cubierto con chile abanero', image:'logo', image2: 'assets/logo.png' }
+                {name:'Manchego', description:'Queso puramente manchego', image: 'assets/manchego.png' },
+                {name:'Enchilado', description:'Queso manchego cubierto con chile abanero', image: 'assets/enchilado.png' },
+                {name:'Chipotle', description:'Queso manchego cubierto con chile abanero', image: 'assets/chipotle.png' },
+                {name:'Epazote', description:'Queso manchego cubierto con chile abanero', image: 'assets/logo.png' }
             ]
         }
     },
---
+++ src/views/ProductsP.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h1>Productos</h1>
+        <h2>Quesos</h2>
+        <b-row>
+            <b-col cols="12" set="12" sm="12" md="6" lg="4" xl="4" v-for="(cheese, index) of cheeses" :key=index>
+                <b-card :title='cheese.name' :img-src="require('@/assets/logo.png')" img-top>
+                    <b-card-text>
+                        {{cheese.description}}
+                    </b-card-text>
+                </b-card>
+                <b-card :title='cheese.name' :img-src="getImagePath(cheese.image)" img-top>
+                    <b-card-text>
+                        {{cheese.description}}
+                    </b-card-text>
+                </b-card>
+                <b-card :title='cheese.name' :img-src="require(`../assets/${cheese.image}.png`)" img-top>
+                    <b-card-text>
+                        {{cheese.description}}
+                    </b-card-text>
+                </b-card>
+                <b-card :title='cheese.name' :img-src="require(`../${cheese.image2}`)" img-top>
+                    <b-card-text>
+                        {{cheese.description}}
+                    </b-card-text>
+                </b-card>
+            </b-col>
+        </b-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Products',
+    data(){
+        return{
+            cheeses:[
+                {name:'Manchego', description:'Queso puramente manchego', image:'logo', image2: 'assets/logo.png' },
+                {name:'Enchilado', description:'Queso manchego cubierto con chile abanero', image:'logo', image2: 'assets/logo.png' }
+            ]
+        }
+    },
+    methods: {
+        getImagePath(imageName){
+            return imageName ? require(`../assets/${imageName}.png`): ''
+        }
+    },
+}
+</script>
